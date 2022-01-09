c8850dafcf2a36ac16d137c94ad8b673ed995bb3
Ram Kinkar Singh <ramkinkar2512@gmail.com>
2022-01-09 14:43:57 +0530

    Update main.dart
    adding Column Widget to list out Question and respective answers.

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,34 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // Application Root.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quiz on Habbit',
+      title: 'QuizApp',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('All about Habit'),
+          title: Text('Welcome to Quiz App'),
         ),
         body: Center(
-          child: Text('Welcome to Quiz'),
-        ),
+            child: Column(
+          children: [
+            Text('Question Statement'),
+            RaisedButton(child: Text('Option 1'), onPressed: null),
+            RaisedButton(child: Text('Option 2'), onPressed: null),
+            RaisedButton(child: Text('Option 3'), onPressed: null),
+            RaisedButton(child: Text('Option 4'), onPressed: null),
+          ],
+        )),
       ),
     );
   }
